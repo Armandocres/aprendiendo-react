b58d0f8c890dcad7ef21229ac49c9fe584cf8fdb
Armandocres <armandocres99@gmail.com>
2023-08-16 16:07:00 -0600

    renderizando listas

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,18 +1,47 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import TwitterFollowCard from './TwitterFollowCard';
 import './App.css'
 
 const App = () => {
+  // eslint-disable-next-line no-unused-vars
   const formatUsername = (userName) => `@${userName}` //pasar funciones como props
 
   const formattedUsername = (<span>@midudev</span>) //pasar elementos como props
 
+  const user = [
+    {
+      userName: 'Midudev',
+      name: 'Míguel Ángel Durán',
+      isFollowing: true
+    },
+    {
+      userName: 'Midudev',
+      name: 'Míguel Ángel Durán',
+      isFollowing: false
+    },
+    {
+      userName: 'Midudev',
+      name: 'Míguel Ángel Durán',
+      isFollowing: true
+    }
+  ]
+
   return (
     <div className='App'>
-      <TwitterFollowCard username='Midudev' name='ArmandoCres' formattedUsername={formattedUsername} initialIsFollowing={true} />
-      <TwitterFollowCard username='Midudev' name='ArmandoCres' formattedUsername={formattedUsername} />
-      <TwitterFollowCard username='Midudev' name='ArmandoCres' formattedUsername={formattedUsername} />
-      <TwitterFollowCard username='Midudev' name='ArmandoCres' formattedUsername={formattedUsername} initialIsFollowing={true} />
+      {user.map(user => {
+        const { userName, name, isFollowing } = user;
+
+        return (
+          <TwitterFollowCard
+            key={name}
+            username={userName}
+            name={name}
+            initialIsFollowing={isFollowing}
+            formattedUsername={formattedUsername}
+           />
+        )
+      })}
     </div>
   )
 }
